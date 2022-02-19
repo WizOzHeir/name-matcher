d17665c9fa10f56045308410b20dd90960f5c507
wizozheir <anastasia.meleshchenko@gmail.com>
2022-02-19 19:22:55 +0200

    revert

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo Building WizOzHeir/name-matcher:build
-docker build --build-arg url=https://github.com/WizOzHeir/name-matcher\
-  --build-arg project=name-matcher\
-  --build-arg artifactid=namematcher\
-  --build-arg version=0.1.0\
-  -t name-matcher - < Dockerfile
